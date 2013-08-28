675b630b06fb632f4b54f93f80c85193955a8c16
Kuldeep Singh Pal <tkuldeep.singh@innoraft.com>
2013-08-28 21:29:13 +0530

    Adds css and remove border from Constituency page
    * Add css for blue button in constituency page
    * Changes js file for removing border in container which do not have any content
    
    reference -> AamAadmiParty/delhi-standalone-drupal#1

--- sites/all/themes/aap_theme/js/constituency-page.js
+++ sites/all/themes/aap_theme/js/constituency-page.js
@@ -4,8 +4,11 @@
 				$(".shs-hierarchy li:nth-child(1)").addClass("state");
 				$(".shs-hierarchy li:nth-child(2)").addClass("district");
 				$(".shs-hierarchy li:nth-child(3)").addClass("constituency");
-				if ( $('.aap-delhi-region .field__item').children().length > 0 ) {
-					 $('.aap-delhi-region .field__item').addClass("aap-delhi-region-content");
+				if ( $('.aap-delhi-region.container-1 .field__item').children().length > 0 ) {
+					 $('.aap-delhi-region.container-1 .field__item').addClass("aap-delhi-region-content");
+				}
+				if ( $('.aap-delhi-region.container-2 .field__item').children().length > 0 ) {
+					 $('.aap-delhi-region.container-2 .field__item').addClass("aap-delhi-region-content");
 				}
 			}
 	}	
